9fefc89eb9924d0cd87f898c2c762b6b83640578
Sam Forrest <samforrest@live.co.uk>
2022-05-17 07:53:07 +0000

    [IMPLEMENT] base template - sidebar WIP

---
+++ static/js/script.js
@@ -0,0 +1,6 @@
+// Initialise Bootstrap tooltips
+
+let tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+let tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
