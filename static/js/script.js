2b5b1f994095f37d2527d2b07ac62c610f75bcf0
Sam Forrest <samforrest@live.co.uk>
2022-05-24 20:06:22 +0000

    [IMPLEMENT] added mobile nav

--- static/js/script.js
+++ static/js/script.js
@@ -1,6 +1,25 @@
 // Initialise Bootstrap tooltips
-
 let tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
 let tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
   return new bootstrap.Tooltip(tooltipTriggerEl)
 });
+
+// mobile nav
+$('.burger-button').click(function() {
+  let sidebar = $('#sidebar');
+  let burgerButton = $(this);
+
+  // add/remove active class to burger button
+  if (burgerButton.hasClass('__active')) {
+    burgerButton.removeClass('__active');
+  } else {
+    burgerButton.addClass('__active');
+  }
+
+  // add/remove active class to nav
+  if (sidebar.hasClass('__active')) {
+    sidebar.removeClass('__active');
+  } else {
+    sidebar.addClass('__active');
+  }
+})
