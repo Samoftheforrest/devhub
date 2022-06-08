02e2c38f828630b8038d967519c360368bde0f3b
Sam Forrest <samforrest@live.co.uk>
2022-06-08 20:20:02 +0000

    [AMEND] added guard clause to email js

--- devhub/static/js/email.js
+++ devhub/static/js/email.js
@@ -1,11 +1,11 @@
-(function() {
-    // https://dashboard.emailjs.com/admin/account
-    emailjs.init('a8B3AGA56UuueD0HR');
-})();
+(() => emailjs.init('a8B3AGA56UuueD0HR'))();
+
+const formElementsList = ['email', 'subject', 'message'];
 
 window.onload = function() {
     document.getElementById('contact-form').addEventListener('submit', function(event) {
         event.preventDefault();
+        if (!this.email.value || !this.subject.value || !this.message.value) return;
         // generate a five digit number for the contact_number variable
         this.contact_number.value = Math.random() * 100000 | 0;
         // these IDs from the previous steps
