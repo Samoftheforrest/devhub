5a03c9a1620f8e55e166bd577bfa628b525fa43d
Sam Forrest <samforrest@live.co.uk>
2022-06-09 20:37:16 +0000

    [AMEND] set up basic routing for profile bio

--- devhub/routes.py
+++ devhub/routes.py
@@ -182,8 +182,9 @@ def go_to_profile(username):
     """
     # grab the session user's username from the database
     username = User.query.filter_by(username=username).first()
+    user = mongo.db.users.find_one({"account_name": str(username)})
     projects = list(mongo.db.projects.find({"created_by": str(username)}))
-    return render_template("pages/profile.html", projects=projects, username=username, profile_active=True)
+    return render_template("pages/profile.html", projects=projects, username=username, user=user, profile_active=True)
 
 
 # edit profile page
