5cfa595f1ab99a1fd2f58248f4ad6223da96b7ab
Sam Forrest <samforrest@live.co.uk>
2022-06-11 14:38:00 +0000

    [FIX] updated uri for heroku databse

--- devhub/__init__.py
+++ devhub/__init__.py
@@ -1,4 +1,5 @@
 import os
+import re
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_pymongo import PyMongo
@@ -16,8 +17,11 @@ app.secret_key = os.environ.get('SECRET_KEY')
 if os.environ.get("DEVELOPMENT") == "True":
     app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_URL')
 else:
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
-    
+    uri = os.environ.get("DATABASE_URL")
+    if uri.startswith("postgres://"):
+        uri = uri.replace("postgres://", "postgresql://")
+    app.config['SQLALCHEMY_DATABASE_URI'] = uri
+
 app.config['UPLOAD_FOLDER'] = "static/uploads/"
 
 db = SQLAlchemy(app)
